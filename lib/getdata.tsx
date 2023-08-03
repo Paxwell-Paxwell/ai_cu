41592d4f9952b2bf091e9065b621cdd3059f48f8
Paxwell-Paxwell <pompom231245@gmail.com>
2023-08-03 08:32:52 +0700

    update /1000 to kwH

--- lib/getdata.tsx
+++ lib/getdata.tsx
@@ -4,6 +4,8 @@ export const getData = async (url:string):Promise<Prediction[]> => {
   try {
     const res = await aiCu.get(url)
     // console.log(res.data)
+    res.data.data.energy/1000
+    res.data.data.peakPower/1000
     return res.data.data
   }
   catch (err) {
